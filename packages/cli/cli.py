77c4e53cd545955674bf44cd575a0e11d88596c0
Mleczorsky <myster.pluszak@gmail.com>
2024-04-25 20:33:24 +0200

    Running simulation from cli fixed

--- packages/cli/cli.py
+++ packages/cli/cli.py
@@ -12,15 +12,16 @@ SEPARATOR = "\n\t> "
 BOTS_STR = SEPARATOR + SEPARATOR.join(BOTS)
 MAPS_STR = SEPARATOR + SEPARATOR.join(MAPS)
 
+
 @click.command()
 @click.option("--b1",
-              prompt=f'Select one of bots:{ BOTS_STR }',
+              prompt=f'Select one of bots:{BOTS_STR}',
               help='Left bot for simulation')
 @click.option("--b2",
-              prompt=f'Select one of bots:{ BOTS_STR }',
+              prompt=f'Select one of bots:{BOTS_STR}',
               help='Right bot for simulation')
 @click.option("--map",
-              prompt=f'Select one of maps: { MAPS_STR }',
+              prompt=f'Select one of maps: {MAPS_STR}',
               help='Map for simulation')
 @click.option("--log_name", default="temp", help='Name of log file')
 @click.option("--games", default=1, help='Number of games to simulate')
@@ -36,21 +37,27 @@ def run_simulation(
         ready_timeout,
         move_timeout,
         game_timeout):
-    print("Simulating")
     simulations = play(
         b1, b2, games, map, log_name,
         ready_timeout, move_timeout, game_timeout
     )
+
+    for i, simulation in enumerate(simulations):
+        print(f'[ {i + 1} / {games} ] > {simulation}')
     print("After simulation")
+
 
 @click.command()
 def run_gui():
     Main()
+
+
 @click.group
 def cli(): pass
 
 
 cli.add_command(run_simulation)
 cli.add_command(run_gui)
+
 if __name__ == "__main__":
     cli()
