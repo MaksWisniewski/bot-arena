ad3e97a8df3ac9bbeec8906c7bbe4bb3140aab9b
stanely1 <sb.reda456@gmail.com>
2024-05-17 15:08:15 +0200

    make RUN.sh a bit faster

--- RUN.sh
+++ RUN.sh
@@ -37,11 +37,11 @@ case $choice in
         ;;
     5)
         # Run sim
-        docker run --rm -v "$(pwd):/usr/src/app" -it "$image_name" zsh -c "python3 main.py run-simulation"
+        docker run --rm -v "$(pwd):/usr/src/app" -it "$image_name" sh -c "python3 main.py run-simulation"
         ;;
     6)
         # Run sim for minmax and random on 5x5
-        docker run --rm -v "$(pwd):/usr/src/app" -it "$image_name" zsh -c "python3 main.py run-simulation --b1 minmax --b2 random_bot.py --map map5x5.json"
+        docker run --rm -v "$(pwd):/usr/src/app" -it "$image_name" sh -c "python3 main.py run-simulation --b1 minmax --b2 random_bot.py --map map5x5.json"
         ;;
     *)
         echo "Invalid option. Please enter a valid number."
